b0be40097156dd5d4e2fb8ad281c35b373c8053d
Grodondo <dragon.carlos@outlook.es>
2024-05-26 00:19:49 +0200

    Added a function that divides the triangles into smaller ones

--- src/tetrahedron/Panel.java
+++ src/tetrahedron/Panel.java
@@ -7,57 +7,27 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
 
 public class Panel {
 
     private static BufferedImage img;
-	
-    static boolean sameSide(Vertice A, Vertice B, Vertice C, Vertice p)
-    {
-    	Vertice V1V2 = new Vertice(B.x - A.x,B.y - A.y,B.z - A.z);
-    	Vertice V1V3 = new Vertice(C.x - A.x,C.y - A.y,C.z - A.z);
-    	Vertice V1P = new Vertice(p.x - A.x,p.y - A.y,p.z - A.z);
-
-        // Si el producto cruzado del vector V1V2 y el vector V1V3 es el mismo que el del vector V1V2 y el vector V1p, están en el mismo lado.
-        // Solo queda juzgar la direccion de z
-        double V1V2CrossV1V3 = V1V2.x * V1V3.y - V1V3.x * V1V2.y;
-        double V1V2CrossP = V1V2.x * V1P.y - V1P.x * V1V2.y;
-
-        return V1V2CrossV1V3 * V1V2CrossP >= 0;
-    }
     
-    public static Color getShade(Color color, double shade) 
-    {
-    		
-        double redLinear = Math.pow(color.getRed(), 2.2) * shade;
-        double greenLinear = Math.pow(color.getGreen(), 2.2) * shade;
-        double blueLinear = Math.pow(color.getBlue(), 2.2) * shade;
-
-        int r = (int) Math.pow(redLinear, 1 / 2.2);
-        int g = (int) Math.pow(greenLinear, 1 / 2.2);
-        int b = (int) Math.pow(blueLinear, 1 / 2.2);
-
-        return new Color(r, g, b);
- 
-//        int r = (int) (color.getRed() * shade);
-//        int g = (int) (color.getGreen() * shade);
-//        int b = (int) (color.getBlue() * shade);
-//        return new Color(r, g, b);
-    }
+    static List<Triangulo> tris = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
         int x[] = {100, 100, 100,  100, 100, 100,  -100,100,-100,  -100, 100, -100};
         int y[] = {-100, -100, 100,  100, -100, -100};
         
-        List<Triangulo> tris = new ArrayList<>();
+
 		
      // Creación de triángulos iniciales
         tris.add(new Triangulo( new Vertice(x[0], x[1], x[2]),
@@ -81,26 +51,27 @@ public class Panel {
 				Color.BLUE));
 		
 
+        for (int i = 0; i < 6; i++) {
+            tris = inflar(tris);
+        }
         
 		// Ventana
         JFrame frame = new JFrame();
         Container plane = frame.getContentPane();
         plane.setLayout(new BorderLayout());
-       
+        
         // Display panel
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
             	
-//                if (img == null) {
-//                    img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-//                }
                 img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
                 
                 
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
-                //g2.translate(getWidth() / 2, getHeight() / 2);
+                
+               
 
                 // Matriz de transformacion para la rotacion en el eje y -> xz
                 double heading = Math.toRadians(x[0]);
@@ -128,7 +99,9 @@ public class Panel {
 	            }
                
                
-               g2.setColor(Color.WHITE);                
+               g2.setColor(Color.WHITE); 
+              
+               
                for (Triangulo t : tris) {
                 	Vertice v1 = transform.transform(t.v1);
                 	Vertice v2 = transform.transform(t.v2);
@@ -160,12 +133,12 @@ public class Panel {
                     // coseno entre el vector normal del triangulo y la direccion de la luz
                     double angleCos = Math.abs(norm.z);
                     
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
-                    path.closePath();
-                    g2.draw(path);
+//                    Path2D path = new Path2D.Double();
+//                    path.moveTo(v1.x, v1.y);
+//                    path.lineTo(v2.x, v2.y);
+//                    path.lineTo(v3.x, v3.y);
+//                    path.closePath();
+//                    g2.draw(path);
                     
                     // Calcula el rango a ser procesado
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
@@ -228,5 +201,65 @@ public class Panel {
         
         
 	}
+	
+	// Comprueba que el Verice P esté en el mismo lado que los otros 3 (que esté dentro del Triangulo)
+    static boolean sameSide(Vertice A, Vertice B, Vertice C, Vertice p)
+    {
+    	Vertice V1V2 = new Vertice(B.x - A.x,B.y - A.y,B.z - A.z);
+    	Vertice V1V3 = new Vertice(C.x - A.x,C.y - A.y,C.z - A.z);
+    	Vertice V1P = new Vertice(p.x - A.x,p.y - A.y,p.z - A.z);
 
+        // Si el producto cruzado del vector V1V2 y el vector V1V3 es el mismo que el del vector V1V2 y el vector V1p, están en el mismo lado.
+        // Solo queda juzgar la direccion de z
+        double V1V2CrossV1V3 = V1V2.x * V1V3.y - V1V3.x * V1V2.y;
+        double V1V2CrossP = V1V2.x * V1P.y - V1P.x * V1V2.y;
+
+        return V1V2CrossV1V3 * V1V2CrossP >= 0;
+    }
+    
+    // reduce la intensidad del color en funcion a la distancia
+    public static Color getShade(Color color, double shade) 
+    {
+    		
+        double redLinear = Math.pow(color.getRed(), 2.2) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.2) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.2) * shade;
+
+        int r = (int) Math.pow(redLinear, 1 / 2.2);
+        int g = (int) Math.pow(greenLinear, 1 / 2.2);
+        int b = (int) Math.pow(blueLinear, 1 / 2.2);
+
+        return new Color(r, g, b);
+ 
+//        int r = (int) (color.getRed() * shade);
+//        int g = (int) (color.getGreen() * shade);
+//        int b = (int) (color.getBlue() * shade);
+//        return new Color(r, g, b);
+    }
+	
+    // 
+    public static List<Triangulo> inflar(List<Triangulo> tris) {
+        List<Triangulo> innerTris = new ArrayList<>();
+        
+        for (Triangulo t : tris) 
+        {
+            Vertice m1 = new Vertice((t.v1.x + t.v2.x)/2, (t.v1.y + t.v2.y)/2, (t.v1.z + t.v2.z)/2);
+            Vertice m2 = new Vertice((t.v2.x + t.v3.x)/2, (t.v2.y + t.v3.y)/2, (t.v2.z + t.v3.z)/2);
+            Vertice m3 = new Vertice((t.v1.x + t.v3.x)/2, (t.v1.y + t.v3.y)/2, (t.v1.z + t.v3.z)/2);
+            innerTris.add(new Triangulo(t.v1, m1, m3, t.color));
+            innerTris.add(new Triangulo(t.v2, m1, m2, t.color));
+            innerTris.add(new Triangulo(t.v3, m2, m3, t.color));
+            innerTris.add(new Triangulo(m1, m2, m3, t.color));
+        }
+        for (Triangulo t : innerTris) {
+            for (Vertice v : new Vertice[] { t.v1, t.v2, t.v3 }) 
+            {
+                double l = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z) / Math.sqrt(30000);
+                v.x /= l;
+                v.y /= l;
+                v.z /= l;
+            }
+        }
+        return innerTris;
+    }
 }
